0c1edcab07f9fa729e7f911cc6f63681c1ffc2f6
Mohammed Umar <umar@nanosoftengineers.com>
2021-04-10 15:40:29 +0530

    nanobuild

---
+++ build/precache-manifest.0a22e41c70edc884f8eedbd72f854720.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e57e2106d90653edf30fe8deb12be4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bb48d7ee25dab7b31fd",
+    "url": "/static/css/main.f220650d.chunk.css"
+  },
+  {
+    "revision": "7b603629328023849ddd",
+    "url": "/static/js/2.c24a2dc3.chunk.js"
+  },
+  {
+    "revision": "acf2b8406b56c66ece3248e1e789058e",
+    "url": "/static/js/2.c24a2dc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bb48d7ee25dab7b31fd",
+    "url": "/static/js/main.f9ae88a9.chunk.js"
+  },
+  {
+    "revision": "c1b53e25dc9ce0b56989",
+    "url": "/static/js/runtime-main.41593023.js"
+  },
+  {
+    "revision": "38ea370b9f21eafee6053d691121b659",
+    "url": "/static/media/smartfm.38ea370b.png"
+  }
+]);
