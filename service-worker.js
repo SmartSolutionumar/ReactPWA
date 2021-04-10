d49bab5548e8a1170de73949622f752d66de9477
Mohammed Umar <umar@nanosoftengineers.com>
2021-04-10 16:51:36 +0530

    tele

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0a22e41c70edc884f8eedbd72f854720.js"
+  "/precache-manifest.720104384c9fdf64fb2121f1a65199c8.js"
 );
 
 self.addEventListener('message', (event) => {
