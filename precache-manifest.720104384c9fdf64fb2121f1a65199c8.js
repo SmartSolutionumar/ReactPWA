f34c3ebdf656c0a1ce7b95ba50df7b84c380a478
Mohammed Umar <umar@nanosoftengineers.com>
2021-04-11 14:26:26 +0530

    tel

---
+++ precache-manifest.720104384c9fdf64fb2121f1a65199c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f7dcd4506b759c4e83d6473f0f2591",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37bf1f4b36722a19a47a",
+    "url": "/static/css/main.b372c50d.chunk.css"
+  },
+  {
+    "revision": "0d891c6cccaaafe4dcca",
+    "url": "/static/js/2.58f2e2c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.58f2e2c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37bf1f4b36722a19a47a",
+    "url": "/static/js/main.d02fad0e.chunk.js"
+  },
+  {
+    "revision": "c1b53e25dc9ce0b56989",
+    "url": "/static/js/runtime-main.41593023.js"
+  }
+]);
